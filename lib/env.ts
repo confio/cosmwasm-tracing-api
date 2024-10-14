7710e9a32084a5c92efee0216d849d4272427dff
Abel Fernández <44572727+abefernan@users.noreply.github.com>
2024-10-14 14:06:52 +0200

    Add page size envvar

--- lib/env.ts
+++ lib/env.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 const envSchema = z.object({
   ELASTIC_NODE_URL: z.string().url(),
   SPAN_INDEX: z.string(),
+  TXS_PAGE_SIZE: z.number({ coerce: true }),
   PORT: z.optional(z.number().positive().int().safe()),
 });
 
